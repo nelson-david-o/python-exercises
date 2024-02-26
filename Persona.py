c2d6d91040f44d70d9432fd8d55d13e77a5445b9
Nelson David <nelso570@gmail.com>
2024-02-25 19:23:52 -0500

    Modulo persona

--- Persona.py
+++ Persona.py
@@ -1,20 +1,46 @@
 class Persona:
 
-# *args para argumentos variables como tupla
-# **args para argumentos variables de tipo diccionario
-    def __init__(self, nombre, apellido, edad, *args, **kwargs):
-        self.nombre = nombre
-        self.apellido = apellido
-        self.edad = edad
-        self.args = args
-        self.kwargs = kwargs
+    def __init__(self, nombre, apellido, edad):
+        self._nombre = nombre
+        self._apellido = apellido
+        self._edad = edad
+
+    @property
+    def nombre(self):
+        print('Llamando metodo get nombre')
+        return self._nombre
+
+    @property
+    def apellido(self):
+        print('Llamando metodo get apaellido')
+        return self._apellido
+
+    @property
+    def edad(self):
+        print('Metodo get edad')
+        return self._edad
+
+    @nombre.setter
+    def update(self, nombre):
+        print('Llamando metodo set nombre')
+        self._nombre = nombre
+
+    @apeliido.setter
+    def updateApellido(self, apellido):
+        self._apellido = apellido
+
+    @edad.setter
+    def updateEdad(self, edad):
+        self._edad = edad
 
     def mostrar_detalle(self):
-        print(f'Persona: {self.nombre} {self.apellido} {self.edad} {self.args} {self.kwargs}')
+        print(f'Persona: {self._nombre} {self.apellido} {self.edad}')
 
-persona1 = Persona('Nelson','Ortiz',25, '311600790', 2,3,5, m='manzana', p='pera')
+persona1 = Persona('Nelson','Ortiz',25)
+print(persona1.nombre)
+print(persona1.apellido)
+print(persona1.edad)
+persona1.update = 'Tony'
+persona1.updateApellido = 'stark'
+persona1.updateEdad = 26
 persona1.mostrar_detalle()
-
-
-persona2 = Persona('Elsa','Fernandez',27)
-persona2.mostrar_detalle()
